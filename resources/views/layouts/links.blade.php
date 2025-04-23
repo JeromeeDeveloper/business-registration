fa8896da3f6b03467752b16f965a7fac65a036c8
JeromeeDeveloper <jerome.porcado11@gmail.com>
2025-04-23 08:52:06 +0800

    added cdn for dropdown

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -11,5 +11,6 @@
     <!-- jQuery Scrollbar -->
     <script src="{{asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
  <script src="{{asset('assets/js/ga.min.js')}}"></script>
