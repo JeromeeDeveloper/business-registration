5de1e3930381aacd608aa8210e98241e5489c0b5
mocryan123 <mocryan122@gmail.com>
2025-04-23 14:24:31 +0800

    fixed the cam

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -1,12 +1,7 @@
   <!--   Core JS Files   -->
   <script src="{{asset('assets/js/core/jquery-3.7.1.min.js')}}"></script>
   <script src="{{asset('assets/js/core/popper.min.js')}}"></script>
-  <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
-
-    <!--   Core JS Files   -->
-    <script src="{{asset('assets/js/core/jquery-3.7.1.min.js')}}"></script>
-    <script src="{{asset('assets/js/core/popper.min.js')}}"></script>
-    <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
+  {{-- <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script> --}}
 
     <!-- jQuery Scrollbar -->
     <script src="{{asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
