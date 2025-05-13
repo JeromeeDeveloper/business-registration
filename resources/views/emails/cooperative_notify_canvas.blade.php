56c1c909d02de90a6d198dd473b714f62743ae2a
JeromeeDeveloper <jerome.porcado11@gmail.com>
2025-05-13 16:16:20 +0800

    fixed

--- resources/views/emails/cooperative_notify_canvas.blade.php
+++ resources/views/emails/cooperative_notify_canvas.blade.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-    
+
     <div class="container" style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
 
         {{-- <img src="https://i.imgur.com/gsBSpUs.jpeg" alt="Banner Image" style="max-width: 70%; height: auto;" /> --}}
@@ -18,23 +18,42 @@
 
             <p>Co-operative greetings from MASS-SPECC!</p>
 
-            <p>You have been successfully registered to the Canvas LMS. Please use the login credentials below to access your account:</p>
+            <p>You have been successfully enrolled in the Virtual School of Cooperation through Canvas LMS. Please use
+                the login credentials below to access your Canvas account:</p>
 
             <h3>Login Credentials:</h3>
 
             <p><strong>Username:</strong> {{ $coop->email }}</p>
             <p><strong>Password:</strong> password123</p>
 
-            <p>You can log in at: <a href="https://mass-specc.instructure.com/" target="_blank">https://mass-specc.instructure.com/</a></p>
+            <p>You can log in at: <a href="https://mass-specc.instructure.com/"
+                    target="_blank">https://mass-specc.instructure.com/</a></p>
 
-            <p>If you need any assistance, feel free to reach out to our Institute of Cooperatives (ICS) Team at <strong>0966 442 5724/ 0917 142 5284/ 0916 135 5984.</strong>. We're here to help!</p>
+            <p>If you need any assistance, feel free to reach out to our Institute of Cooperatives (ICS) Team at
+                <strong>0966 442 5724/ 0917 142 5284/ 0916 135 5984.</strong> We're here to help!</p>
 
             <p><em>This is a system-generated email. Please do not reply.</em></p>
+
+            <p>__</p>
+            <p>Cooperatively Yours,</p>
+            <p><b><em>ICS Team</em></b></p>
+
+            <div style="margin: 0; padding: 0; line-height: 1.2;">
+                <h2 style="margin: 0;">Institute of Cooperative Studies</h2>
+                <p style="margin: 0;">MASS-SPECC Cooperative Development Center</p>
+                <p style="margin: 0;">Tiano-Yacapin Streets, Cagayan de Oro City, PHILIPPINES 9000</p>
+                <p style="margin: 0;">Telefax No.: +63 88 855-3343</p>
+                <p style="margin: 0;">Mobile No.: +63 916 135 5984 +63 917 142 5284 +63 966 442 5724</p>
+                <p style="margin: 0;">Visit us at: <a href="http://www.mass-specc.coop">mass-specc.coop</a></p>
+            </div>
+
+
         </div>
 
         <hr>
 
-        <img src="https://ci3.googleusercontent.com/mail-sig/AIorK4wvLE7Z259KZkYLTl4fj54VtU7p-zQ4jjAB9ZXkSAwUOFgAfT6LwPgnqjsOXo9LpNSC6IojBRszQ4Mm" alt="Signature Image" style="max-width: 250px; height: auto;" />
+        <img src="https://ci3.googleusercontent.com/mail-sig/AIorK4wvLE7Z259KZkYLTl4fj54VtU7p-zQ4jjAB9ZXkSAwUOFgAfT6LwPgnqjsOXo9LpNSC6IojBRszQ4Mm"
+            alt="Signature Image" style="max-width: 250px; height: auto;" />
 
         <p style="font-size: 0.9em; color: #666;">
             The contents of this email message and any attachments are intended solely for the addressee(s) and may
@@ -42,7 +61,8 @@
             the sender immediately and delete this message.
         </p>
 
-        <img src="https://ci3.googleusercontent.com/mail-sig/AIorK4y3f7vZ8XrGXNcxDBf4tYVurPLjYeeZt2kNny2Bwwzvi8OL66nmQ7o4LvVFkTijuDgTLV6e3DdOI0oA" alt="Second Signature Image" style="max-width: 250px; height: auto;" />
+        <img src="https://ci3.googleusercontent.com/mail-sig/AIorK4y3f7vZ8XrGXNcxDBf4tYVurPLjYeeZt2kNny2Bwwzvi8OL66nmQ7o4LvVFkTijuDgTLV6e3DdOI0oA"
+            alt="Second Signature Image" style="max-width: 250px; height: auto;" />
 
     </div>
 </body>
