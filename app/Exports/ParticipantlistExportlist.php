31cacb55d0e1653fb38b37d2a0761dc33e9f925d
JeromeeDeveloper <jerome.porcado11@gmail.com>
2025-05-21 17:44:33 +0800

    added fix

--- app/Exports/ParticipantlistExportlist.php
+++ app/Exports/ParticipantlistExportlist.php
@@ -26,7 +26,7 @@ class ParticipantlistExportlist implements FromCollection, WithHeadings, ShouldA
             ->map(function ($participant) {
                 return [
                     'Participant Name' => trim("{$participant->first_name} {$participant->middle_name} {$participant->last_name}"),
-                    'Phone Number' => "'" . preg_replace('/^0/', '+63', $participant->phone_number), // Treat as text
+                    'Phone Number' => "'" . preg_replace('/^0/', '63', $participant->phone_number), // Treat as text
                 ];
             });
     }
